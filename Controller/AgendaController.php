a645ab1d91d19d18fecd3e3442c540ef1648b301
masando <masando@edemy.es>
2015-10-06 16:49:31 +0200

    main frontpage modules

--- Controller/AgendaController.php
+++ Controller/AgendaController.php
@@ -116,7 +116,7 @@ class AgendaController extends BaseController
 
     public function onFrontpageModuleNamespace_Actividades(ContentEvent $event)
     {
-        if($this->getParam('frontpage_module.actividades.enable') == 1) {
+        if($this->getParam('frontpage_module_namespace.actividades.enable') == 1) {
             //$this->get('edemy.meta')->setTitlePrefix("Catálogo");
             $categories = $this->getRepository('eDemyAgendaBundle:Actividad')->findAllOrdered($this->getNamespace(), true);
 
@@ -128,7 +128,7 @@ class AgendaController extends BaseController
 
     public function onFrontpageModuleNamespace_Products(ContentEvent $event)
     {
-        if($this->getParam('frontpage_module.product.enable') == 1) {
+        if($this->getParam('frontpage_module_namespace.product.enable') == 1) {
             //$this->get('edemy.meta')->setTitlePrefix("Catálogo");
             $products = $this->getRepository('eDemyProductBundle:Product')->findAllFavorites($this->getNamespace(), $this->get('doctrine.orm.entity_manager'), 'destacado');
 
